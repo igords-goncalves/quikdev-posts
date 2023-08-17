eba1556fe4edf8db454ad3cc4672c6ec9421fa9d
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-17 09:53:18 -0300

    feat: criação do componente input pattern e passagem de propriedades do react hook form

---
+++ src/components/Form/FormInput.tsx
@@ -0,0 +1,28 @@
+/**
+ * Adjusting correctly the types of the input props
+ */
+interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+  register: any;
+}
+
+export const FormInput: React.FC<FormInputProps> = ({
+  label,
+  register,
+  ...props
+}) => {
+  return (
+    <div className="flex flex-col w-full my-4 items-start">
+      <label className="text-sm font-normal text-gray-300">
+        {label}
+        <sup>*</sup>
+      </label>
+      <input
+        className="px-4 py-2 border border-gray-400 rounded-md"
+        {...props}
+        {...register}
+      />
+    </div>
+  );
+};
