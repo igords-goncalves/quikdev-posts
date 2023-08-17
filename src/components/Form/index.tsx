fab39f347cbae2ba6780c37c02a49350fbb8f5e3
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-17 09:51:49 -0300

    feat: import dos components patterns e definindo um objeto de confirguração

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,11 @@
+import { FormRoot } from './FormRoot';
+import { FormTitle } from './FormTitle';
+import { FormInput } from './FormInput';
+import { FormSubmit } from './FormSubmit';
+
+export const Form = {
+  Root: FormRoot,
+  Title: FormTitle,
+  Input: FormInput,
+  Submit: FormSubmit,
+};
