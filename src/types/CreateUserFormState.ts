91768a8198438c70bba163c3aa123ed4cc4004fe
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-17 18:58:35 -0300

    docs: adição de comentário

--- src/types/CreateUserFormState.ts
+++ src/types/CreateUserFormState.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
 import { createUserFormSchema } from '../schemas/createUserFormSchema';
 
+// Função z.infer é usada para inferir esses tipos reais com base no esquema.
 export type CreateUserFormState = z.infer<typeof createUserFormSchema>;
