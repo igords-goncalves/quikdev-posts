460dafab61b41d4d87d8fbbcf1bcfa3296501eec
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-18 11:27:32 -0300

    refactor: criação de esquemas para forms login e registro no mesmo arquivo

--- src/schemas/createUserFormSchema.ts
+++ src/schemas/createUserFormSchema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 
-export const createUserFormSchema = z.object({
+export const loginFormSchema = z.object({
+  username: z.string().nonempty({ message: 'Usuário é obrigatório' }),
+  password: z.string().nonempty({ message: 'Senha é obrigatória' }),
+});
+
+export const registerFormSchema = z.object({
   name: z.string().nonempty({ message: 'Nome é obrigatório' }),
   username: z.string().nonempty({ message: 'Usuário é obrigatório' }),
   password: z.string().nonempty({ message: 'Senha é obrigatória' }),
