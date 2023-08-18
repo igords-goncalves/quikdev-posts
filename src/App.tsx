1336557c6a5fb809ef5abb7a30bdebde58f832c8
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-18 16:26:02 -0300

    feat: import do componente ToastContainer e CSS referente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './routes/Routes';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
       <Routes />
+      <ToastContainer position="top-center" theme="colored" />
     </>
   );
 }
