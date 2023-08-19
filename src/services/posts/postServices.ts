6241232e4aa77067639585d37b86730f73c7f8fc
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-19 01:59:44 -0300

    feat: criação de arquivo com serviços de requisção de posts

---
+++ src/services/posts/postServices.ts
@@ -0,0 +1,40 @@
+import FakerApi from '../api/fakerApi';
+import { toast } from 'react-toastify';
+
+const fakeApi = new FakerApi();
+
+export const getPost = async () => {
+  try {
+    const response: any = await fakeApi.get('/posts', {});
+    if (response.success) {
+      return response.data;
+    }
+  } catch (error: unknown | any) {
+    toast.error(error.message);
+  }
+};
+
+export const createPost = async (title: string, content: string) => {
+  try {
+    const response: any = await fakeApi.post('/posts/create', {
+      title,
+      content,
+    });
+    if (response.success) {
+      toast.success(response.message);
+    }
+  } catch (error: unknown | any) {
+    console.log('an error has occurred', error.message);
+  }
+};
+
+export const removePost = async (post_id: number) => {
+  try {
+    const response: any = await fakeApi.delete('/posts/remove', { post_id });
+    if (response.success) {
+      toast.success(response.message);
+    }
+  } catch (error: unknown | any) {
+    toast.error(error.message);
+  }
+};
