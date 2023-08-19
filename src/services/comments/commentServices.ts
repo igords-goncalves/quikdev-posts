61dd2b1bf4c944412f092ec275d3a8a0758e2865
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-19 12:11:00 -0300

    feat: criação dos serviços e métodos de requisição dos comentários

---
+++ src/services/comments/commentServices.ts
@@ -0,0 +1,62 @@
+import FakerApi from '../api/fakerApi';
+import { toast } from 'react-toastify';
+
+const fakeApi = new FakerApi();
+
+export const getComment = async (post_id: number) => {
+  try {
+    const response: any = await fakeApi.get('/comments', { post_id });
+    if (response.success) {
+      return response.data;
+    }
+  } catch (error: unknown | any) {
+    toast.error(error.message);
+  }
+};
+
+export const createComment = async (title: string, content: string) => {
+  try {
+    const response: any = await fakeApi.post('/comments/create', {
+      title,
+      content,
+    });
+    if (response.success) {
+      toast.success(response.message);
+    }
+  } catch (error: unknown | any) {
+    console.log('an error has occurred', error.message);
+  }
+};
+
+export const removeComment = async (post_id: number, comment_id: number) => {
+  try {
+    const response: any = await fakeApi.delete('/comments/remove', {
+      post_id,
+      comment_id,
+    });
+    if (response.success) {
+      toast.success(response.message);
+    }
+  } catch (error: unknown | any) {
+    toast.error(error.message);
+  }
+};
+
+export const editComment = async (
+  post_id: number,
+  comment_id: number,
+  post: { title: string; content: string },
+) => {
+  try {
+    const response: any = await fakeApi.put('/comments/update', {
+      post_id,
+      comment_id,
+      post,
+    });
+    if (response.success) {
+      toast.success(response.message);
+    }
+  } catch (error: unknown | any) {
+    toast.error(error.message);
+  }
+};
