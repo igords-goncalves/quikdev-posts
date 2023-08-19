bf130bf6119d5e869139bcaeb2a4c60821e274f8
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-19 02:04:09 -0300

    feat: criação do arquivo de imports dos componets patterns

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,19 @@
+import { PostActionComment } from './PostActionComent';
+import { PostArea } from './PostArea';
+import { PostCard } from './PostCard';
+import { PostContainer } from './PostContainer';
+import { PostHeder } from './PostHeader';
+import { PostRoot } from './PostRoot';
+import { PostSubmit } from './PostSubmit';
+import { PostInput } from './PostTitle';
+
+export const Post = {
+  Root: PostRoot,
+  Input: PostInput,
+  Container: PostContainer,
+  TextArea: PostArea,
+  Submit: PostSubmit,
+  Card: PostCard,
+  Header: PostHeder,
+  Comment: PostActionComment,
+};
