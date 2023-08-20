7ed7812b1ebedbf403e11691027eaf5235c13ea6
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-20 13:48:21 -0300

    refactor: exclusão de componentes patterns desnecessários

--- src/components/Post/PostRoot.tsx
+++
@@ -1,9 +0,0 @@
-interface PostRootProps {
-  children: React.ReactNode;
-}
-
-export const PostRoot: React.FC<PostRootProps> = ({ children }) => {
-  return (
-    <div className="flex flex-col items-start justify-between">{children}</div>
-  );
-};
