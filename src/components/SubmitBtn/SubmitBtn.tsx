1c5f63470d4f9eb3649105ca2636cdf1329d2c54
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-20 23:03:54 -0300

    refactor: troca de nome do componente tipo botão submit

---
+++ src/components/SubmitBtn/SubmitBtn.tsx
@@ -0,0 +1,16 @@
+interface SubmitBtnProps {
+  children?: React.ReactNode;
+}
+
+const SubmitBtn: React.FC<SubmitBtnProps> = ({ children }) => {
+  return (
+    <button
+      type="submit"
+      className="border-[#6038f1] border-2 hover:bg-[#4026a0] text-white text-sm mx-4 my-4 px-6 py-2 rounded-md mt-4"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default SubmitBtn;
